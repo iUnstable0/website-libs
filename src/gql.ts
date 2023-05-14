51a44b9144a3f3cb484b45fc1b4ec213366ed400
iUnstable0 <iunstable0@gmail.com>
2023-05-14 18:03:49 +0700

    update

--- src/gql.ts
+++ src/gql.ts
@@ -1,16 +1,40 @@
 export default class lib_gql {
   public static combineQueries(...queries) {
-    const combinedQueryContent = queries
-      .map((query) => {
-        const lines = query.trim().split("\n");
-        const contentLines = lines.slice(1, -1);
-        return contentLines.map((line) => line.trim()).join("\n");
-      })
-      .join("\n");
+    const combinedQueryVariables = {};
+    const rawQueries = [];
+
+    queries.forEach((query) => {
+      let queryVariables;
+
+      if (query.includes("$")) {
+        queryVariables = query
+          .substring(query.indexOf("(") + 1, query.indexOf(")"))
+          .trim();
+      }
+
+      if (queryVariables) {
+        queryVariables.split(",").forEach((variable) => {
+          const [key, value] = variable.split(":");
+          combinedQueryVariables[key.trim()] = value.trim();
+        });
+      }
+
+      const rawQuery = query
+        .substring(query.indexOf("{") + 1, query.lastIndexOf("}"))
+        .trim();
+
+      rawQueries.push(rawQuery);
+    });
 
     return `
-        query {
-            ${combinedQueryContent}
+        query${
+          Object.keys(combinedQueryVariables).length
+            ? `(${Object.keys(combinedQueryVariables)
+                .map((key) => `${key}: ${combinedQueryVariables[key]}`)
+                .join(", ")})`
+            : ""
+        } {
+            ${rawQueries.join("\n")}
         }
     `;
   }
